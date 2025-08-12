ffaa41be3c45dafb24311fe2e1cd3596f468baba
guiizidoro <guisilva120407@gmail.com>
2025-08-12 16:11:59 -0300

    Update generate_database.sh
    Wait and try again

--- Docker/scripts/generate_database.sh
+++ Docker/scripts/generate_database.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Source the environment variables unconditionally,
-# since the Docker build will have the .env file available.
+# Source environment variables unconditionally.
 source ./.env
 
-# The rest of the script remains the same.
 source ./Docker/scripts/env_functions.sh
 
 if [ "$DOCKER_ENV" != "true" ]; then
@@ -15,13 +13,26 @@ if [[ "$DATABASE_PROVIDER" == "postgresql" || "$DATABASE_PROVIDER" == "mysql" ||
     export DATABASE_URL
     echo "Generating database for $DATABASE_PROVIDER"
     echo "Database URL: $DATABASE_URL"
-    npm run db:generate
-    if [ $? -ne 0 ]; then
-        echo "Prisma generate failed"
-        exit 1
-    else
-        echo "Prisma generate succeeded"
-    fi
+
+    # We add a retry loop to give the database time to start.
+    RETRY_COUNT=0
+    MAX_RETRIES=10
+    
+    while [ $RETRY_COUNT -lt $MAX_RETRIES ]; do
+        npm run db:generate
+        if [ $? -eq 0 ]; then
+            echo "Prisma generate succeeded"
+            exit 0 # Success, exit the script.
+        else
+            RETRY_COUNT=$((RETRY_COUNT+1))
+            echo "Prisma generate failed, retrying in 5 seconds... ($RETRY_COUNT/$MAX_RETRIES)"
+            sleep 30 # Wait for 5 seconds before retrying.
+        fi
+    done
+
+    echo "Error: Prisma generate failed after $MAX_RETRIES retries."
+    exit 1 # Failure, exit the script with an error.
+
 else
     echo "Error: Database provider $DATABASE_PROVIDER invalid."
     exit 1

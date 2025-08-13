9ed0115c7b860b294146ec7022c302d04d5f4af0
guiizidoro <guisilva120407@gmail.com>
2025-08-13 10:25:27 -0300

    Update env_functions.sh

--- Docker/scripts/env_functions.sh
+++ Docker/scripts/env_functions.sh
@@ -1,18 +1,18 @@
 export_env_vars() {
-    if [ -f .env ]; then
-        while IFS='=' read -r key value; do
-            if [[ -z "$key" || "$key" =~ ^\s*# || -z "$value" ]]; then
-                continue
-            fi
-
-            key=$(echo "$key" | tr -d '[:space:]')
-            value=$(echo "$value" | tr -d '[:space:]')
-            value=$(echo "$value" | tr -d "'" | tr -d "\"")
-
-            export "$key=$value"
-        done < .env
-    else
-        echo ".env file not found"
+    if [ ! -f .env ]; then
+        echo "❌ .env file not found. Exiting."
         exit 1
     fi
+
+    while IFS='=' read -r key value; do
+        if [[ -z "$key" || "$key" =~ ^\s*# || -z "$value" ]]; then
+            continue
+        fi
+
+        key=$(echo "$key" | tr -d '[:space:]')
+        value=$(echo "$value" | tr -d '[:space:]')
+        value=$(echo "$value" | tr -d "'" | tr -d "\"")
+
+        export "$key=$value"
+    done < .env
 }
